47511f45bc0d93c06901b0cd34e604beefa0a381
Jorge Berrizbeitia <58472035+jorgeberrizbeitia@users.noreply.github.com>
2022-02-16 16:50:41 +0100

    lecture done

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const app = express()
 
 const lessonsData = require("./someData")
 
-
 // configuracion
 app.set('view engine', 'hbs');
 // cuando trabajamos con motores de plantillas, usamos .render()
 // layout será la base para TODAS las vistas que busquemos
 app.set("views", __dirname + "/views")
+
+let hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+const DogApi = require('doggo-api-wrapper');
+const myDog = new DogApi();
 
 app.get('/', (req, res) => {
   // console.log(process.env.CLAVE_SECRETA)
@@ -44,7 +50,7 @@ app.get("/approved-lessons", (req, res) => {
 // params
 app.get("/lessons-per-bootcamp/:bootcamp", (req, res) => {
 
-  console.log(req.params.bootcamp)
+  console.log(req.params) // parametros que se pasan por el URL
 
   const filteredLessons = lessonsData.filter((eachLesson) => {
     return eachLesson.bootcamp === req.params.bootcamp
@@ -53,14 +59,14 @@ app.get("/lessons-per-bootcamp/:bootcamp", (req, res) => {
   res.render("lessons.hbs", {
     lessonsKeyName: filteredLessons
   })
-
 })
 
 // queries
 app.get("/search", (req, res) => {
 
-  console.log(req.query)
+  console.log(req.query) // queries que se pasan por inputs => /?search=algo
 
+  // find funciona como el filter pero solo retorna un elemento.
   const searchLesson = lessonsData.find((eachElem) => {
     return eachElem.topic === req.query.search
   })
@@ -68,8 +74,57 @@ app.get("/search", (req, res) => {
   res.render("search.hbs", {
     searchLesson: searchLesson
   })
+})
+
+
+// Rutas de la API
+app.get("/dog", (req, res) => {
+
+  myDog.getARandomDog()
+  .then((data) => {
+    console.log(data) // SIEMPRE REVISAN COMO VIENE LA DATA DE LA API
+    res.render("dogs/random-dog.hbs", { data } )
+    // {
+    //   data: data
+    // }
+    // ???
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+
+  // ??? data??
+  // console.log(data)
 
 })
+
+app.get("/dog-by-breed/:breed", (req, res) => {
+
+  myDog.getAllDogsByBreed(req.params.breed)
+  .then(data => {
+    console.log(data)
+    res.render("dogs/dog-by-breed.hbs", { data })
+  })
+  .catch(err => console.error(err))
+
+})
+
+app.get("/breed", (req, res) => {
+
+  myDog.getListOfAllBreeds()
+  .then(data => {
+    const breeds = Object.keys(data.message)
+    console.log(breeds)
+    res.render("dogs/breeds.hbs", { breeds })
+
+  })
+  .catch(err => console.error(err))
+
+
+})
+
+
+
 
 
 app.listen(process.env.PORT, () => {
